f8ed86626ee0ae963904e5c03905edd6244b0ebc
mahm0udelshahat <mahmoudelshahat595@gmail.com>
2024-07-10 10:45:49 +0300

    edit Changes to be committed:

--- lib/signup.dart
+++ lib/signup.dart
@@ -12,177 +12,181 @@ class Signup extends StatelessWidget {
         children: [
           Container(
             width: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  "SIGNUP",
-                  style: TextStyle(
-                    fontSize: 17,
-                    fontFamily: "MyFont",
-                    fontWeight: FontWeight.bold
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                SvgPicture.asset(
-                  "assets/icons/signup.svg",
-                  width: 200,
-                ),
-                SizedBox(
-                  height: 50,
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.purple[100],
-                    borderRadius: BorderRadius.circular(66),
-                  ),
-                  width: 266,
-                  padding: EdgeInsets.symmetric(horizontal: 16),
-                  child: TextField(
-                    decoration: InputDecoration(
-                        icon: Icon(
-                          Icons.person,
-                          color: Colors.purple[800],
-                          size: 19,
-                        ),
-                        hintText: "example@mail.com",
-                        border: InputBorder.none),
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.purple[100],
-                    borderRadius: BorderRadius.circular(66),
-                  ),
-                  width: 266,
-                  padding: EdgeInsets.symmetric(horizontal: 16),
-                  child: TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        suffix: Icon(
-                          Icons.visibility,
-                          color: Colors.purple[900],
-                        ),
-                        icon: Icon(
-                          Icons.lock,
-                          color: Colors.purple[800],
-                          size: 19,
-                        ),
-                        hintText: "Password",
-                        border: InputBorder.none),
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                ElevatedButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.purple),
-                      padding: MaterialStateProperty.all(
-                          EdgeInsets.symmetric(horizontal: 92, vertical: 10)),
-                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(27))),
+            child: SingleChildScrollView(
+              padding: EdgeInsets.symmetric(vertical: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    "SIGNUP",
+                    style: TextStyle(
+                      fontSize: 17,
+                      fontFamily: "MyFont",
+                      fontWeight: FontWeight.bold
                     ),
-                    child: Text(
-                      "SIGNUP",
-                      style: TextStyle(
-                        fontSize: 24,
-                        color: Colors.white,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  SvgPicture.asset(
+                    "assets/icons/signup.svg",
+                    width: 200,
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.purple[100],
+                      borderRadius: BorderRadius.circular(66),
+                    ),
+                    width: 266,
+                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          icon: Icon(
+                            Icons.person,
+                            color: Colors.purple[800],
+                            size: 19,
+                          ),
+                          hintText: "example@mail.com",
+                          border: InputBorder.none),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.purple[100],
+                      borderRadius: BorderRadius.circular(66),
+                    ),
+                    width: 266,
+                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          suffix: Icon(
+                            Icons.visibility,
+                            color: Colors.purple[900],
+                          ),
+                          icon: Icon(
+                            Icons.lock,
+                            color: Colors.purple[800],
+                            size: 19,
+                          ),
+                          hintText: "Password",
+                          border: InputBorder.none),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Colors.purple),
+                        padding: MaterialStateProperty.all(
+                            EdgeInsets.symmetric(horizontal: 92, vertical: 10)),
+                        shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(27))),
                       ),
-                    )),
-                SizedBox(
-                  height: 35,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Already have account? "),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pushNamed(context, "/login");
-                      },
                       child: Text(
-                        "Log in",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    )
-                  ],
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                SizedBox(
-                  width: 300,
-                  child: Row(
+                        "SIGNUP",
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.white,
+                        ),
+                      )),
+                  SizedBox(
+                    height: 35,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Expanded(
-                          child: Divider(
-                        color: Colors.purple[200],
-                      )),
-                      Text("OR"),
-                      Expanded(
-                          child: Divider(
-                        color: Colors.purple[200],
-                      )),
+                      Text("Already have account? "),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pop(context);
+                          Navigator.pushNamed(context, "/login");
+                        },
+                        child: Text(
+                          "Log in",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      )
                     ],
                   ),
-                ),
-                SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      margin: EdgeInsets.symmetric(horizontal: 10),
-                      padding: EdgeInsets.all(16),
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.purple, width: 0),
-                        shape: BoxShape.circle,
-                      ),
-                      child: SvgPicture.asset(
-                        "assets/icons/facebook.svg",
-                        width: 22,
-                        color: Colors.purple[500],
-                      ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  SizedBox(
+                    width: 300,
+                    child: Row(
+                      children: [
+                        Expanded(
+                            child: Divider(
+                          color: Colors.purple[200],
+                        )),
+                        Text("OR"),
+                        Expanded(
+                            child: Divider(
+                          color: Colors.purple[200],
+                        )),
+                      ],
                     ),
-                    Container(
-                      margin: EdgeInsets.symmetric(horizontal: 10),
-                      padding: EdgeInsets.all(16),
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.purple, width: 0),
-                        shape: BoxShape.circle,
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 10),
+                        padding: EdgeInsets.all(16),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.purple, width: 0),
+                          shape: BoxShape.circle,
+                        ),
+                        child: SvgPicture.asset(
+                          "assets/icons/facebook.svg",
+                          width: 22,
+                          color: Colors.purple[500],
+                        ),
                       ),
-                      child: SvgPicture.asset(
-                        "assets/icons/twitter.svg",
-                        width: 22,
-                        color: Colors.purple[500],
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 10),
+                        padding: EdgeInsets.all(16),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.purple, width: 0),
+                          shape: BoxShape.circle,
+                        ),
+                        child: SvgPicture.asset(
+                          "assets/icons/twitter.svg",
+                          width: 22,
+                          color: Colors.purple[500],
+                        ),
                       ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.symmetric(horizontal: 10),
-                      padding: EdgeInsets.all(16),
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.purple, width: 0),
-                        shape: BoxShape.circle,
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 10),
+                        padding: EdgeInsets.all(16),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.purple, width: 0),
+                          shape: BoxShape.circle,
+                        ),
+                        child: SvgPicture.asset(
+                          "assets/icons/google-plus.svg",
+                          width: 22,
+                          color: Colors.purple[500],
+                        ),
                       ),
-                      child: SvgPicture.asset(
-                        "assets/icons/google-plus.svg",
-                        width: 22,
-                        color: Colors.purple[500],
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 80,
-                )
-              ],
+                    ],
+                  ),
+                  SizedBox(
+                    height: 80,
+                  )
+                ],
+              ),
             ),
           ),
           Positioned(
